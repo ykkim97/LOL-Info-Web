338566b834f694d6d47ad606f34dbf02ca5903f0
YoungGwon Kim <97ykkim@naver.com>
2022-10-30 21:52:00 +0900

    API 요청 테스트 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import GetData from './components/GetData';
+
+const API_KEY = 'RGAPI-670c28b3-44fa-4190-9b61-384446f6a273';
 
 function App() {
   return (
     <div className="App">
-      LOL 전적검색!
+      <h1>LOL 전적</h1>
+      <GetData API_KEY={API_KEY}/>
     </div>
   );
 }
