2e8c6699ddeaff7ede3f9549f1d8847debc79d9e
ykkim97 <97ykkim@gmail.com>
2022-11-03 18:32:05 +0900

    특정챔피언의 이미지를 불러오도록 수정

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [gameList, setGameList] = useState([]); // 매치정보가 들어갈 Array
   const [leagueList, setLeagueList] = useState([]); // 소환사 리그정보(티어정보)가 들어갈 Array
 
+  // 매치정보를 가져오는 함수
   const getPlayerGames = (e) => {
     axios.get('http://localhost:4000/past5Games', {params : {searchText : searchText}}) // params 추가(검색기능)
       .then((response) => {
@@ -18,8 +19,7 @@ function App() {
       .catch((error) => console.log(error));
   }
 
-  console.log(gameList); // 매치 정보
-
+  // 소환사 리그정보를 가져오는 함수
   const getPlayerLeague = (e) => {
     axios.get('http://localhost:4000/tier', {params : {searchText : searchText}})
       .then((response) => {
@@ -28,13 +28,15 @@ function App() {
       .catch((error) => console.log(error));
   }
 
-  console.log(leagueList) // 소환사 리그정보(티어정보)
-  console.log(championData[0]); // 챔피언데이터가 들어있는 Array
+  console.log(gameList); // 매치 정보
+  console.log(leagueList, "leagueList") // 소환사 리그정보(티어정보)
+  console.log(championData, "championData"); // 챔피언데이터가 들어있는 Array
 
   return (
     <div className={styles["App"]}>
       <div className={styles['search-container']}>
         <h1>LOLY.GG</h1>
+        
         <input 
           type="text" 
           className={styles['app-searchBox']}
@@ -51,7 +53,7 @@ function App() {
           <FcSearch className={styles['app-searchButton-icons']}/>
         </button>
       </div>
-      
+      <img src={'https://ddragon.leagueoflegends.com/cdn/10.6.1/img/champion/Syndra.png'}/> {/* 특정챔피언의 이미지를 불러오는데는 성공 */}
       <Match gameList={gameList} searchText={searchText} leagueList={leagueList}/>
     </div>
   );
