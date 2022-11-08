cea57ea23b1a6fb58fecb27217b999b6524da36c
YoungGwon Kim <97ykkim@naver.com>
2022-11-08 23:53:29 +0900

    챔피언별 상세정보 컴포넌트 템플릿 생성

--- src/pages/ChampionInfo.js
+++ src/pages/ChampionInfo.js
@@ -5,15 +5,21 @@ const ChampionInfo = (
     championData
 ) => {
     const values = Object.values(championData.championData[0])
-    // console.log(values[3], "values")
+    console.log(values[3], "values")
     const name = Object.keys(values[3])
+    const test = Object.values(values[3])
     // console.log(name)
+    // console.log(test)
+
     return (
         <>
             <h3 className={styles['championInfo-title']}>챔피언</h3>
             <div className={styles['championInfo-container']}>
-                {name.map(championName => (
-                    <div className={styles['championInfo-champContainer']}>
+                {name.map((championName, index) => (
+                    <div 
+                        className={styles['championInfo-champContainer']} 
+                        key={index}
+                    >
                         <img 
                             src={`https://ddragon.leagueoflegends.com/cdn/10.6.1/img/champion/${championName}.png`} 
                             className={styles['championInfo-img']}
