979fe10fed322ba247dc970a9a15fb70d84f20a3
YoungGwon Kim <97ykkim@naver.com>
2022-12-20 00:18:41 +0900

    ChampionInfo.js의 챔피언리스트를 외부파일로 분리

---
+++ src/components/contents/ChampionInfo/ChampionListComponent.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./ChampionListComponent.module.css";
+
+const ChampionListComponent = ({
+    onErrorImg,
+    championObjectArray,
+}) => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            {championObjectArray.map((championName, index) => {
+                return (
+                    <div 
+                        className={styles['championList-champContainer']} 
+                        key={index}
+                        onClick={() => {navigate(`/championInfo/${championObjectArray[index].key}`)}}
+                    >
+                        <img 
+                            src={`https://ddragon.leagueoflegends.com/cdn/12.21.1/img/champion/${championObjectArray[index].id}.png`} 
+                            className={styles['championList-img']}
+                            onError={() => onErrorImg()}
+                            alt={championObjectArray[index].id}
+                        />
+                        <div>{championObjectArray[index].name}</div>
+                    </div>
+                )})
+            }
+        </>
+    )
+}
+
+export default ChampionListComponent;
