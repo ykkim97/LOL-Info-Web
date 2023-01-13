17e0f3f98be621eb5e30a98f370061e45a3d2842
YoungGwon Kim <97ykkim@naver.com>
2023-01-13 22:05:17 +0900

    ActiveGames 컴포넌트 개발 수정중

--- src/components/contents/Home/ActiveGames.js
+++ src/components/contents/Home/ActiveGames.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import styles from "./ActiveGames.module.css";
-import { useSelector } from "react-redux";
+import ChampionData from "../../../Data/Champions/ChampionData";
 
 const ActiveGames = ({
     activeGames,
@@ -10,17 +10,55 @@ const ActiveGames = ({
         console.log(activeGames, "activeGames")
     }, [])
 
+    const championDataArray = Object.values(ChampionData[0].data); // 모든챔피언데이터 Object를 Array로 변환
+    const bannedChampionArray = []; // 밴된 챔피언 이미지경로값을 담을 Array
+    const playingChampionArray = []; // 플레이중인 챔피언을 담을 Array
+
     return (
         <div>
             {typeof activeGames !== 'object' 
                 ? <div className={styles['activeGames-Container']}>현재 게임중이 아닙니다.</div> 
                 : <div className={styles['activeGames-Container']}>
-                    <h3>{activeGames.gameMode}</h3>
-                    {activeGames.participants?.map(participant => (
-                        <>
-                            <div className={styles[`participant-name`]}>{participant.summonerName}</div>
-                        </>
-                    ))}
+                    {/* 게임 모드 */}
+                    <h3 id={styles['gameMode']}>
+                        {activeGames.gameMode}
+                    </h3>
+                    {/* 밴픽 */}
+                    <div className={styles['banned-Container']}>
+                        {activeGames.bannedChampions?.map((champion, index) => {
+                            championDataArray.forEach((obj) => {
+                                if (obj.key == champion.championId) {
+                                    bannedChampionArray.push(obj.image.full);
+                                }
+                            })
+                            return (
+                                <img 
+                                    src={`https://ddragon.leagueoflegends.com/cdn/12.21.1/img/champion/${bannedChampionArray[index]}`}
+                                    onError={onErrorImg}
+                                    className={styles['banned-Champions']}
+                                />
+                            )
+                        })}
+                    </div>
+                    {/* 참가자 정보 */}
+                    {activeGames.participants?.map((participant, index) => {
+                        championDataArray.forEach((obj) => {
+                            if (obj.key == participant.championId) {
+                                playingChampionArray.push(obj.image.full);
+                            }
+                        })
+                        return (
+                            <div className={styles['participant-Container']}>
+                                <img 
+                                    src={`https://ddragon.leagueoflegends.com/cdn/12.21.1/img/champion/${playingChampionArray[index]}`}
+                                    onError={onErrorImg}
+                                    className={styles['playing-Champions']}
+                                />
+                                <div className={styles[`participant-name`]}>{participant.summonerName}</div>
+                                {/* TODO: 참가자 랭크정보를 얻을 수 있는 방법 찾기 */}
+                            </div>
+                        )
+                    })}
                 </div>
             }
         </div>
